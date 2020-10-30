28291a9e80a959df1da4c79960efd5e8852df0ec
Aritra Bhattacharjee <whoisaritra@gmail.com>
2020-10-30 21:08:42 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import FormBase from './pages/FormBase'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        Hello World!
+        <Route exact path='/form' component={FormBase}/>
+        <Route  render={() => {<div>Default Route</div>}}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/FormBase.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import {
+    makeStyles,
+    Modal,
+    Backdrop,
+    Fade
+} from '@material-ui/core'
+
+//Components
+import FormFieldCreator from '../components/FormFieldCreator'
+
+const useStyles = makeStyles((theme) => ({
+    modal : {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+}))
+
+const FormBase = () => {
+    const classes = useStyles()
+    const [ formSkeleton, updateFormSkeleton ] = useState([])
+    const [ modalState, changeModalState ] = useState(false)
+
+
+    const modalOpenHandler = () => {
+        changeModalState(true)
+    }
+
+    const modalCloseHandler = () => {
+        changeModalState(false)
+    }
+
+    const addFormField = (fieldType) => {
+        updateFormSkeleton([...formSkeleton, { type: 'text', id: formSkeleton.length === 0 ? 0 : formSkeleton[formSkeleton.length - 1].id + 1 }])
+        console.log(formSkeleton)
+    }
+
+    const removeField = (id) => {
+        const newSkeleton = formSkeleton.filter((elem) => elem.id !== id)
+        updateFormSkeleton(newSkeleton)
+    }
+
+    
+    return(
+        <div>
+            {/* Render the Form Here */}
+            <div>
+                { formSkeleton.map(field => {
+                    switch(field.type){
+                        case 'text':
+                            return (<div key={field.id}>
+                                        <input type="text" placeholder={field.id}/>
+                                        <button onClick={() => removeField(field.id)}>Remove this</button>
+                                    </div>)
+                        default:
+                            return <div>Shit {field.id}</div>
+                    }
+                })}
+            </div>
+
+            {/* Configuring the Modal */}
+            <Modal
+                className={classes.modal}
+                open={modalState}
+                BackdropComponent={Backdrop}
+                BackdropProps={{ timeout: 500 }}
+                onClose={modalCloseHandler}
+                // closeAfterTransition
+            >
+                <Fade in={modalState}>
+                    <FormFieldCreator addFieldHandler={addFormField} modalCloseHandler={modalCloseHandler} />
+                </Fade>
+            </Modal>
+
+            {/* Button To Update the Form */}
+            <div>
+                <button type="button" onClick={modalOpenHandler}>Add TextField</button>
+            </div>
+        </div>
+    )
+}
+
+export default FormBase;
